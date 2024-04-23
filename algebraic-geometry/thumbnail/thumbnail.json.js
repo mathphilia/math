b04852e81469961bf11e964a6b817c72a458cfd6
mathphilia <104004553+mathphilia@users.noreply.github.com>
2024-04-24 01:17:20 +0900

    edit

--- algebraic-geometry/thumbnail/thumbnail.json.js
+++ algebraic-geometry/thumbnail/thumbnail.json.js
@@ -1,3 +1,3 @@
 var pages = JSON.parse(
-    '{"pageName":["prerequisite","Noetherian"],"pageTitle":["前提知識・参考文献等","Noether環とNoether加群"]}'
+    '{"pageName":["prerequisite","Noetherian","integral-extension"],"pageTitle":["前提知識・参考文献等","Noether環とNoether加群","整拡大とZariskiの補題"]}'
 );
