38906b60baa8150e1bca7877ef346f8be53bd0ed
mathphilia <104004553+mathphilia@users.noreply.github.com>
2024-04-28 19:45:21 +0900

    edit

--- algebraic-geometry/thumbnail/thumbnail.json.js
+++ algebraic-geometry/thumbnail/thumbnail.json.js
@@ -1,3 +1,3 @@
 var pages = JSON.parse(
-    '{"pageName":["prerequisite","Noetherian","integral-extension"],"pageTitle":["前提知識・参考文献等","Noether環とNoether加群","整拡大とZariskiの補題"]}'
+    '{"pageName":["prerequisite","Noetherian","algebraic-set","integral-extension","Nullstellensatz"],"pageTitle":["前提知識・参考文献等","Noether環とNoether加群","代数的集合","整拡大とZariskiの補題","Hilbertの零点定理"]}'
 );
