4fc88e89ce1cc4dc76e7ca2fb4a70bed6e5b86fa
mathphilia <104004553+mathphilia@users.noreply.github.com>
2025-09-15 22:00:28 +0900

    edit

--- algebraic-geometry/thumbnail/thumbnail.json.js
+++ algebraic-geometry/thumbnail/thumbnail.json.js
@@ -1,3 +1,3 @@
 var pages = JSON.parse(
-    '{"pageName":["prerequisite","Noetherian","affine-space","integral-extension","Nullstellensatz","dimension-1","dimension-2","dimension-variety","sheaf"],"pageTitle":["前提知識・参考文献等","Noether環とNoether加群","アフィン空間の代数的集合","整拡大とZariskiの補題","Hilbertの零点定理","環の次元 (次元論①)","上昇定理・下降定理 (次元論②)","アフィン多様体の次元","層"],"sections":[1,7,1]}'
+    '{"pageName":["prerequisite","Noetherian","affine-space","integral-extension","Nullstellensatz","dimension-1","dimension-2","dimension-variety","projective-space","sheaf"],"pageTitle":["前提知識・参考文献等","Noether環とNoether加群","アフィン空間の代数的集合","整拡大とZariskiの補題","Hilbertの零点定理","環の次元 (次元論①)","上昇定理・下降定理 (次元論②)","アフィン多様体の次元","層"],"sections":[1,7,1]}'
 );
